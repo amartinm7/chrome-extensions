fcc109c91c643267f5385b1f83fdb979a574045c
antonio.martin <antonio.martin@schibsted.com>
2022-01-10 15:33:52 +0100

    query for environment feature

--- extensions/milAdDetail/background.js
+++ extensions/milAdDetail/background.js
@@ -1,9 +1,16 @@
 const color = '#3aa757';
 const SEARCH_API_GATEWAY_ENCODED = 'aHR0cHM6Ly9zZWFyY2hhcGkuZ3cubWlsYW51bmNpb3MuY29t'
 const SEARCH_API_GATEWAY = atob(SEARCH_API_GATEWAY_ENCODED)
+const SEARCH_API_GATEWAY_PRE_ENCODED = 'aHR0cHM6Ly9zZWFyY2hhcGkuZ3cubWlsYW51bmNpb3MubmV0'
+const SEARCH_API_GATEWAY_PRE = atob(SEARCH_API_GATEWAY_PRE_ENCODED)
+
+const getEnvironment = function(env) {
+    return (!!env) ? SEARCH_API_GATEWAY : SEARCH_API_GATEWAY_PRE
+}
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse){
     if (request.message === 'OPEN_TAB'){
-        await chrome.tabs.create( { url: `${SEARCH_API_GATEWAY}/v1/ads/?ids=${request.adId}`} )
+        const env = getEnvironment(request.env)
+        await chrome.tabs.create( { url: `${env}/v1/ads/?ids=${request.adId}`} )
     }
 })
--- extensions/milAdDetail/content-script.js
+++ extensions/milAdDetail/content-script.js
@@ -1,12 +1,19 @@
 // document.body.style.backgroundColor = 'orange';
 
+const MA_DOTCOM_ENCODED = 'bWlsYW51bmNpb3MuY29t'
+const MA_DOTCOM = atob(MA_DOTCOM_ENCODED)
+
 const getAdId = function (text) {
   if (text.includes('r')) return text.substring(1)
   return text
 }
 
+const getEnvironment = function (){
+  return document.querySelector('[rel="canonical"]').href.includes(MA_DOTCOM)
+}
+
 document.addEventListener('click', async function(event) {
   if (event.target.className === 'ma-AdCard-adId') {
-    chrome.runtime.sendMessage({ message: 'OPEN_TAB', adId: getAdId(event.target.innerText) })
+    chrome.runtime.sendMessage({ message: 'OPEN_TAB', adId: getAdId(event.target.innerText), env: getEnvironment() })
   }
 })
